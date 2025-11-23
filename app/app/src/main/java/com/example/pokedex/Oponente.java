b2254d1de2f3d58559e38bac027b39fbfc5cd61f
Thiago Lazari <thiagolazari123@hotmail.com>
2025-11-23 17:55:57 -0300

    Projeto inicial

---
+++ app/app/src/main/java/com/example/pokedex/Oponente.java
@@ -0,0 +1,5 @@
+package com.example.pokedex;
+
+public class Oponente {
+    // Classe básica para implementação futura do oponente
+}
---
+++ app/app/src/main/java/com/example/pokedex/Pokemon.java
@@ -0,0 +1,7 @@
+package com.example.pokedex;
+
+public class Pokemon {
+    public String name;
+    public String type;
+    public String imageUrl;
+}
